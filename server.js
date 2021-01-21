b421a8cb9dc35b0b5b5f33f5017c67a4f38562ef
Petteri <petu_97@hotmail.com>
2021-01-21 15:40:11 +0200

    port updated

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(express.json());
 
 var listJson = require("./list.json");
 
-app.listen("8888", "localhost", () => {
+app.listen(process.env.PORT || 8888, "localhost", () => {
   console.log("Listening on port 8888");
 });
 
